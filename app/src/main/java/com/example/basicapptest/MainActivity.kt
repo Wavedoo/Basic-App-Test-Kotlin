2c8605ba53be187034546f23150ee6d8d271ba6c
Sterling <sterling1@hotmail.ca>
2023-03-21 02:23:38 -0400

    Made a screenshots folder

--- app/src/main/java/com/example/basicapptest/MainActivity.kt
+++ app/src/main/java/com/example/basicapptest/MainActivity.kt
@@ -2,21 +2,19 @@ package com.example.basicapptest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var calculateButton: Button = findViewById(R.id.calculateButton)
-        var resultText: TextView = findViewById(R.id.result)
-        var gradeText: EditText = findViewById(R.id.grade)
-        var weightText: EditText = findViewById(R.id.weight)
+        val calculateButton: Button = findViewById(R.id.calculateButton)
+        val resultText: TextView = findViewById(R.id.result)
+        val gradeText: EditText = findViewById(R.id.grade)
+        val weightText: EditText = findViewById(R.id.weight)
 
         resultText.text = getString(R.string.result, 0.0)
         calculateButton.setOnClickListener{
@@ -33,7 +31,6 @@ class MainActivity : AppCompatActivity() {
             var result = 0.0
             if(grade.toInt() != 0 && weight.toInt() != 0)
                 result = grade * (weight / 100)
-            Log.d("BUTTONS", "User tapped the button. ${result}")
             resultText.text = getString(R.string.result, result)
         }
     }
